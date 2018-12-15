4fcf40cbed5d2976a131668bdf2137b50635f590
hyuraku <yu@hill-wood.com>
2018-12-16 02:24:14 +0900

    move to a section1

---
+++ section1/problem10.rb
@@ -0,0 +1,20 @@
+a_list = [2]
+
+for i in 3..2000000
+  time = 0
+  for ele in a_list
+    if ele > i/2
+      break
+    end
+    if i%ele == 0
+      time += 1
+      break
+    end
+  end
+  if time == 0
+    a_list.push(i)
+  end
+end
+
+# p a_list
+p a_list.sum
