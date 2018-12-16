93a0f35d415db9fe9ab931770268bf9f587aacaf
hyuraku <yu@hill-wood.com>
2018-12-16 11:06:58 +0900

    add other solution for problem

--- section1/problem3.rb
+++ section1/problem3.rb
@@ -1,9 +1,13 @@
 target = 600851475143
-a_list = []
-for num in 2..100000
-  if target % num == 0
-    target = target/num
-    a_list.push(num)
-  end
-end
-p a_list
+# a_list = []
+# for num in 2..100000
+#   if target % num == 0
+#     target = target/num
+#     a_list.push(num)
+#   end
+# end
+# p a_list
+
+require 'prime'
+
+p (Prime.prime_division(target).map {|p, e| [p] * e }.flatten).max
