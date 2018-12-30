b13221a47a8b96769ac6bc41555e73ce1259430e
hyuraku <yu@hill-wood.com>
2018-12-30 22:24:59 +0900

    add file for problem31

---
+++ section4/problem31.rb
@@ -0,0 +1,23 @@
+ways = 8
+
+(0..1).each do |h_i|
+  (0..3).each do |ft_i|
+    (0..9).each do |tt_i|
+      (0..19).each do |ten_i|
+        (0..39).each do |five_i|
+          (0..99).each do |two_i|
+            (0..199).each do |one_i|
+              total = one_i * 1 + two_i * 2 + five_i * 5 + ten_i * 10 + tt_i * 20 + ft_i * 50 + h_i * 100
+              if total == 200
+                p [ one_i,two_i,five_i,ten_i,tt_i,ft_i,h_i]
+                ways += 1
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+p ways
