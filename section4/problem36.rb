adb71ae61cad5c4f12c411cbccb77cfa3f32bc09
hyuraku <yu@hill-wood.com>
2019-01-01 00:55:46 +0900

    make one function

--- section4/problem36.rb
+++ section4/problem36.rb
@@ -1,10 +1,15 @@
 goal = 100 * 10000
 ans = 0
 
+def check_reverce(x)
+  x = x.split('')
+  x == x.reverse
+end
+
 (1..goal).each do |ele|
-  if (ele.to_s).split('') == (ele.to_s.split('')).reverse
+  if check_reverce(ele.to_s)
     bi_ele = ele.to_s(2)
-    if bi_ele.split('') == (bi_ele.split('')).reverse
+    if check_reverce(bi_ele)
       ans += ele
     end
   end
