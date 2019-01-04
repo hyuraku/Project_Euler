3d7d7e19163c1c2b6028422c0f7ded156783608b
hyuraku <yu@hill-wood.com>
2019-01-04 12:27:02 +0900

    add file for problem58

---
+++ section6/problem58.rb
@@ -0,0 +1,38 @@
+require "prime"
+
+def prime_check(n)
+  if Prime.prime?(n)
+    1
+  else
+    0
+  end
+end
+
+ans = 0
+
+sum = 0
+
+a_list = ((1..100000).map{|i| i**2 if i%2==1 }).compact
+use_list = []
+
+a_list.each do |ele|
+  if ele == 1
+    use_list.push(ele)
+  else
+    diff = (Math.sqrt(ele) - 1).to_i
+    use_list.push(ele, ele - diff, ele - diff*2, ele - diff*3)
+    [ele, ele - diff, ele - diff*2, ele - diff*3].each do |list_i|
+      sum += prime_check(list_i)
+    end
+    p sum
+    p use_list.size
+    p sum/(use_list.size).to_f
+    p '////'
+    if sum/(use_list.size).to_f < 0.1
+      p sum
+      p use_list.size
+      p Math.sqrt(ele)
+      break
+    end
+  end
+end
