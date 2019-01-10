e4ba88ad772fd48aebe727a22afd5ece8c4a7d4a
hyuraku <yu@hill-wood.com>
2019-01-11 01:03:49 +0900

    add file for problem24

---
+++ section3/problem24.rb
@@ -0,0 +1,21 @@
+def factorial(n)
+  return 1 if n == 0
+  return n * factorial(n - 1)
+end
+
+goal = 100 * 10000 - (factorial(9) * 2 + factorial(8) * 6 + factorial(7)*6 )
+
+order = 0
+
+(2780134569..9876543210).each do |target|
+  tar = (target.to_s).split('')
+  tar = tar.uniq
+  if tar.size == 10
+    order += 1
+    p order
+  end
+  if order == goal
+    p target
+    break
+  end
+end
