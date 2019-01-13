0ad6bcd9833d5c03327160a3511c9852e0dfd561
hyuraku <yu@hill-wood.com>
2019-01-13 19:07:34 +0900

    make a new class

--- section1/problem6.rb
+++ section1/problem6.rb
@@ -1,9 +1,19 @@
-sum_of_squares = 0
-square_of_sum = 0
+class Problem6
+  def initialize(start, goal)
+    @start = start
+    @goal = goal
 
-for i in 1..100
-  sum_of_squares += i * i
-  square_of_sum += i
+    @sum_of_squares = 0
+    @square_of_sum = 0
+  end
+
+  def run
+    (@start..@goal).each do |i|
+      @sum_of_squares += i * i
+      @square_of_sum += i
+    end
+    @square_of_sum * @square_of_sum - @sum_of_squares
+  end
 end
 
-p square_of_sum * square_of_sum - sum_of_squares
+p Problem6.new(1, 100).run
