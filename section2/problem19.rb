412b00465c6a01ec590e7dead926202c8767a4ff
hyuraku <yu@hill-wood.com>
2019-02-25 23:28:12 +0900

    make a class 'Problem19'

--- section2/problem19.rb
+++ section2/problem19.rb
@@ -1,13 +1,21 @@
 require "Date"
 
-start_day = Date.new(1901,1,1)
-end_day = Date.new(2000,12,31)
-total = 0
+class Problem19
+  def initialize
+    @start_day = Date.new(1901,1,1)
+    @end_day = Date.new(2000,12,31)
+    @total = 0
+  end
 
-(start_day..end_day).each do |day|
-  if day.wday == 0 && day.day == 1
-    total += 1
+  def run
+    (@start_day..@end_day).each do |day|
+      if day.wday == 0 && day.day == 1
+        @total += 1
+      end
+    end
+    @total
   end
 end
 
-p total
+
+p Problem19.new.run
