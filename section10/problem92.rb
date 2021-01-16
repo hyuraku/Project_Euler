3521b48e35e541f345ceb6558c60725f2c2d68c5
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-01-16 21:12:11 +0900

    solve the problem92

--- section10/problem92.rb
+++ section10/problem92.rb
@@ -1,3 +1,7 @@
+def sort_num(x)
+  x.to_s.split('').sort.join("").to_i
+end
+
 def every_num_double(x)
   x.to_s.split('').map{|i| (i.to_i)**2}.sum
 end
@@ -8,17 +12,22 @@ max = 10000000
 list_1 = []
 list_89 = []
 
-(min...max).each do |c|
-  a = every_num_double(c)
+arr = (min...max)
+arr = arr.map{ |i| sort_num(i) }.sort
+arr =  arr.group_by(&:itself).transform_values(&:count)
+
+arr.each do | c , _ |
+  a = c
   while !list_89.include?(a) && !list_1.include?(a) && ![1,89].include?(a)
     a = every_num_double(a)
+    a = sort_num(a)
   end
   if a == 89 || list_89.include?(a)
     list_89.append(c)
   elsif a == 1 || list_1.include?(a)
     list_1.append(c)
-  end    
+  end
 end
 
-p list_89.size
-p list_1.size
+p arr.select{|i,size| list_89.include?(i)}.values.sum
+
