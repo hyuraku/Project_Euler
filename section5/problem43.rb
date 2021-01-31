292989aeec809d9aed0c122e9c23644f58064aa5
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-01-31 21:33:13 +0900

    repair

--- section5/problem43.rb
+++ section5/problem43.rb
@@ -9,38 +9,22 @@ aa = aa.select { |a|
   a > (10 ** 9)
 }
 
+primes = [17,13,11,7,5,3,2]
+
 sum = 0
 aa.each do |a|
-  sixth_num = a%1000
-  if sixth_num%17 != 0
-    next
+  res = true
+  primes.each.with_index(0) do |prime, index|
+    target = a% (10**(index+3)) / (10**(index))
+    if (target%prime != 0)
+      res = false
+      break
+    end
   end
-  fifth_num = (a%10000)/10
-  if fifth_num%13 != 0
-    next
+  if res
+    p a
+    sum += a
   end
-  fourth_num = (a%100000)/100
-  if fourth_num%11 != 0
-    next
-  end
-  third_num = (a%1000000)/1000
-  if third_num%7 != 0
-    next
-  end
-  second_num = (a%10000000)/10000
-  if second_num%5 != 0
-    next
-  end
-  first_num = (a%100000000)/100000
-  if first_num%3 != 0
-    next
-  end
-  init_num = (a%1000000000)/1000000
-  if init_num%2 != 0
-    next
-  end
-  p a
-  sum += a
 end
 
 p sum
